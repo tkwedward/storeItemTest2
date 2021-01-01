e87d3b4f188792668c6b7b06815c531b4fae5fd0
Ed <tkwedward@gmail.com>
2020-12-31 20:00:10 -0800

    Initial Commit

---
+++ SearchStoreBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchStoreBar
+//
+//  Created by Edward Tang on 12/31/20.
+//  Copyright © 2020 Edward Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
